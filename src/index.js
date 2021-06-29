d10abc03131efafdb3956d95171c2dd76d1fd4da
Nizami Dzhumaev <nizamidzhumaev@MacBook-Pro-Nizami.local>
2021-06-29 16:47:17 +0400

    add build

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={ store }>
-      <BrowserRouter>
+      <BrowserRouter basename='https://dzhumaevn.github.io/constructor_new/build/'>
         <AppContainer />
       </BrowserRouter>
     </Provider>
